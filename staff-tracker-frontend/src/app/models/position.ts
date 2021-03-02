68942e52986e649d27ad91df31171abfeb6edce4
noel-castillo <noelcastillo@C02DQ7WHML7H.local>
2021-03-02 18:01:23 -0500

    Can see lists of all employees and all positions with decent frontend.

--- staff-tracker-frontend/src/app/models/position.ts
+++ staff-tracker-frontend/src/app/models/position.ts
@@ -1,15 +1,23 @@
 export class Position {
     id: number;
     title: string;
+    employeeId: number;
+    startTime: string;
+    endTime: string;
     enabled: boolean;
 
     constructor(
         id?: number,
         title?: string,
-        enabled?: boolean
+        employeeId?: number,
+        startTime?: string,
+        endTime?: string
+
     ) {
         this.id = id;
         this.title = title;
-        this.enabled = enabled;
+        this.employeeId = employeeId;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 }
