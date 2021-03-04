e26d32166d4d033ba9a117709528e23dd98683fa
noel-castillo <noelcastillo@C02DQ7WHML7H.local>
2021-03-04 13:52:13 -0500

    Implemented front end for add/edit/delete an employee.

--- staff-tracker-frontend/src/app/components/employees/employees.component.ts
+++ staff-tracker-frontend/src/app/components/employees/employees.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee';
 import { EmployeeService } from 'src/app/services/employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employees',
@@ -10,11 +11,70 @@ import { EmployeeService } from 'src/app/services/employee.service';
 export class EmployeesComponent implements OnInit {
 
   employees: Employee[];
-  addEmployee: boolean = false;
+  showAddEmployeeForm: boolean = false;
+  showEditEmployeeForm: boolean = false;
+  newEmployee: Employee = new Employee();
+  employeeToEdit: Employee;
 
-  constructor(private empService: EmployeeService) { }
+  constructor(private empService: EmployeeService, private router: Router) { }
 
   ngOnInit(): void {
+    this.empService.getAllEmployees().subscribe(list => {
+      this.employees = list
+    });
+  }
+
+  addEmployee(): void {
+    console.log(this.newEmployee);
+    this.showAddEmployeeForm = false;
+    this.empService.addEmployee(this.newEmployee).subscribe(
+      (aGoodThingHappened) => {
+        console.log(aGoodThingHappened);
+        this.newEmployee = new Employee();
+        this.reload();
+      },
+      (didntWork) => {
+        console.error('Employee Component addEmployee() DID NOT WORK');
+        this.reload();
+      }
+    );
+  }
+
+  showEdit(employee: Employee){
+    this.showEditEmployeeForm = true;
+    this.employeeToEdit = employee;
+  }
+
+  editEmployee(): void {
+    this.showEditEmployeeForm = false;
+    this.empService.editEmployee(this.employeeToEdit, this.employeeToEdit.id).subscribe(
+      (aGoodThingHappened) => {
+        console.log(aGoodThingHappened);
+        this.reload();
+      },
+      (didntWork) => {
+        console.error('Employee Component editEmployee(employee) DID NOT WORK');
+        this.reload();
+      }
+    );
+
+  }
+
+  deleteEmployee(id: number): void {
+    this.empService.deleteEmployee(id).subscribe(
+      (aGoodThingHappened) => {
+        console.log(aGoodThingHappened);
+        this.reload();
+      },
+      (didntWork) => {
+        console.error('Employee Component deleteEmployee(id) DID NOT WORK');
+        this.reload();
+      }
+    );
+
+  }
+
+  reload(): void {
     this.empService.getAllEmployees().subscribe(list => {
       this.employees = list
     });
