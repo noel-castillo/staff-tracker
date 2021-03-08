3a6a0fcce675c8a2bfcc4b256f5835cc0818a772
noel-castillo <noelcastillo@C02DQ7WHML7H.local>
2021-03-07 23:08:34 -0500

    Basic CRUD service for Day on frontend.

--- staff-tracker-frontend/src/app/services/day.service.ts
+++ staff-tracker-frontend/src/app/services/day.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Employee } from '../models/employee';
+import { Observable } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { Day } from '../models/day';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DayService {
+    baseURL: string = "http://localhost:8080/api";
+  httpOptions = { headers: new HttpHeaders({ "Content-Type": "application/json" }) };
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  addDay(toAdd: Day): Observable<Day> {
+    return this.http.post<Day>(this.baseURL + "/days/add-day", toAdd, this.httpOptions)
+      .pipe(
+        tap(x => console.log(x)),
+        catchError(err => {
+          console.log(err);
+          return of(null);
+        })
+      );
+  }
+
+  getDay(id: number): Observable<Day> {
+    return this.http.get<Day>(this.baseURL + "/day/" + id)
+      .pipe(
+        tap(x => console.log(x)),
+        catchError(err => {
+          console.log(err);
+          return of(null);
+        })
+      );
+  }
+
+  getDays(): Observable<Day[]> {
+    return this.http.get<Day[]>(this.baseURL + "/days")
+      .pipe(
+        tap(x => console.log(x)),
+        catchError(err => {
+          console.log(err);
+          let empty: Day[] = [];
+          return of(empty);
+        })
+      );
+  }
+
+  editDay(day: Day, id: number): Observable<Day> {
+    return this.http.put<Day>(this.baseURL + "/days/" + id, day, this.httpOptions)
+      .pipe(
+        tap(x => console.log(x)),
+        catchError(err => {
+          console.log(err);
+          return of(null);
+        })
+      );
+  }
+
+  deleteDay(id: number): Observable<Day> {
+    return this.http.delete<Day>(this.baseURL + "/days/delete/" + id, this.httpOptions)
+      .pipe(
+        tap(x => console.log(x)),
+        catchError(err => {
+          console.log(err);
+          return of(null);
+        })
+      );
+  }
 }
