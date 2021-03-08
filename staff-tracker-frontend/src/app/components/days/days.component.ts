b660f826840aa2237252c51912a27c42c3196551
noel-castillo <noelcastillo@C02DQ7WHML7H.local>
2021-03-08 00:45:12 -0500

    Basic admin CRUD on frontend for Day table.

--- staff-tracker-frontend/src/app/components/days/days.component.ts
+++ staff-tracker-frontend/src/app/components/days/days.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Day } from 'src/app/models/day';
+import { Router } from '@angular/router';
+import { DayService } from 'src/app/services/day.service';
 
 @Component({
   selector: 'app-days',
@@ -7,9 +10,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DaysComponent implements OnInit {
 
-  constructor() { }
+  days: Day[];
+  showAddDayForm: boolean = false;
+  showEditDayForm: boolean = false;
+  newDay: Day = new Day();
+  dayToEdit: Day;
+
+  constructor(private dayService: DayService, private router: Router) { }
 
   ngOnInit(): void {
+    this.dayService.getDays().subscribe(list => {
+      this.days = list
+    });
+  }
+
+  addDay(): void {
+    console.log(this.newDay);
+    this.showAddDayForm = false;
+    this.dayService.addDay(this.newDay).subscribe(
+      (aGoodThingHappened) => {
+        console.log(aGoodThingHappened);
+        this.newDay = new Day();
+        this.reload();
+      },
+      (didntWork) => {
+        console.error('Day Component addDay() DID NOT WORK');
+        this.reload();
+      }
+    );
+  }
+
+  showEdit(day: Day) {
+    this.showEditDayForm = true;
+    this.dayToEdit = day;
+  }
+
+  editDay(): void {
+    this.showEditDayForm = false;
+    this.dayService.editDay(this.dayToEdit, this.dayToEdit.id).subscribe(
+      (aGoodThingHappened) => {
+        console.log(aGoodThingHappened);
+        this.reload();
+      },
+      (didntWork) => {
+        console.error('Day Component editDay(day) DID NOT WORK');
+        this.reload();
+      }
+    );
+
+  }
+
+  deleteDay(id: number): void {
+    this.dayService.deleteDay(id).subscribe(
+      (aGoodThingHappened) => {
+        console.log(aGoodThingHappened);
+        this.reload();
+      },
+      (didntWork) => {
+        console.error('Day Component deleteDay(id) DID NOT WORK');
+        this.reload();
+      }
+    );
+
+  }
+
+  reload(): void {
+    this.dayService.getDays().subscribe(list => {
+      this.days = list
+    });
   }
 
 }
